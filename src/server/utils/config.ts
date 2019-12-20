7be674319c17bfd9db62fbbce074de31178048d4
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2019-12-20 15:59:58 +0100

    clean config again

--- src/server/utils/config.ts
+++ src/server/utils/config.ts
@@ -1,7 +1,4 @@
-export const trusted: string[] = [
-  "0x47e172f6cfb6c7d01c1574fa3e2be7cc73269d95",
-  "0xa0af2e71cecc248f4a7fd606f203467b500dd53b"
-]
+export const trusted: string[] = []
 
 export const banned: string[] = []
 
