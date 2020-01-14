e9bd09893ddefecb61223113ca983d2ed152fa2a
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-01-14 15:01:51 +0100

    separate sanity checks from actual logic

--- src/server/utils/isAuthorized.ts
+++ src/server/utils/isAuthorized.ts
@@ -18,68 +18,71 @@ export function isAuthorized(
 
   if (
     isInputValid(stats) &&
-    proof && proof.publicKey && proof.signature &&
-    reserved.indexOf(stats.id) < 0 && trusted
-      .map(address => address && address.toLowerCase())
-      .indexOf(proof.address) >= 0
+    proof && proof.publicKey && proof.signature
   ) {
+    if (
+      reserved.indexOf(stats.id) < 0 && trusted
+        .map(address => address && address.toLowerCase())
+        .indexOf(proof.address) >= 0
+    ) {
 
-    // check that msg hash is equal to msg hash from proof
-    const msgHash = hash(JSON.stringify(stats))
+      // check that msg hash is equal to msg hash from proof
+      const msgHash = hash(JSON.stringify(stats))
 
-    if (!(msgHash === proof.msgHash.substr(2))) {
-      console.error(
-        'API', 'SIG',
-        'Message hash did not match',
-        msgHash, proof.msgHash.substr(2)
-      )
-      return false
-    }
-
-    // recover key from public key
-    const pubkeyNoZeroX = proof.publicKey.substr(2)
-
-    let pubkey: KeyPair
-
-    try {
-      pubkey = secp256k1.keyFromPublic(pubkeyNoZeroX, 'hex')
-    } catch (err) {
-      console.error('API', 'SIG', 'Public Key Error', err.message)
-      return false
-    }
-
-    // compare address
-    const addressHash = hash(pubkeyNoZeroX.substr(2), 'hex')
-      .substr(24)
-
-    if (!(addressHash.toLowerCase() === proof.address.substr(2).toLowerCase())) {
-      console.error(
-        'API', 'SIG',
-        'Address hash did not match', addressHash,
-        proof.address.substr(2)
-      )
-      return false
-    }
-
-    // create signature
-    const signature: {
-      r: string
-      s: string
-    } = {
-      r: proof.signature.substr(2, 64),
-      s: proof.signature.substr(66, 64)
-    }
-
-    // validate sig
-    try {
-      isAuthorized = pubkey.verify(msgHash, signature)
-      if (!isAuthorized) {
-        console.error('API', 'SIG', 'Signature did not verify')
+      if (!(msgHash === proof.msgHash.substr(2))) {
+        console.error(
+          'API', 'SIG',
+          'Message hash did not match',
+          msgHash, proof.msgHash.substr(2)
+        )
         return false
       }
-    } catch (e) {
-      console.error('API', 'SIG', 'Signature Error', e.message)
-      return false
+
+      // recover key from public key
+      const pubkeyNoZeroX = proof.publicKey.substr(2)
+
+      let pubkey: KeyPair
+
+      try {
+        pubkey = secp256k1.keyFromPublic(pubkeyNoZeroX, 'hex')
+      } catch (err) {
+        console.error('API', 'SIG', 'Public Key Error', err.message)
+        return false
+      }
+
+      // compare address
+      const addressHash = hash(pubkeyNoZeroX.substr(2), 'hex')
+        .substr(24)
+
+      if (!(addressHash.toLowerCase() === proof.address.substr(2).toLowerCase())) {
+        console.error(
+          'API', 'SIG',
+          'Address hash did not match', addressHash,
+          proof.address.substr(2)
+        )
+        return false
+      }
+
+      // create signature
+      const signature: {
+        r: string
+        s: string
+      } = {
+        r: proof.signature.substr(2, 64),
+        s: proof.signature.substr(66, 64)
+      }
+
+      // validate sig
+      try {
+        isAuthorized = pubkey.verify(msgHash, signature)
+        if (!isAuthorized) {
+          console.error('API', 'SIG', 'Signature did not verify')
+          return false
+        }
+      } catch (e) {
+        console.error('API', 'SIG', 'Signature Error', e.message)
+        return false
+      }
     }
   } else {
     console.error(
